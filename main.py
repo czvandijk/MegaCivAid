57c6d8e5c6ec92850a65846b3f2a08dbae17f694
chrisvandijk <mail@chrisvandijk.nl>
2024-08-27 20:29:22 +0200

    Change name of sidebar

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import streamlit as st
 
+def main():
+    # builds the sidebar menu
+    with st.sidebar:
+        st.page_link('main.py', label='Welcome', icon='🔥')
+        st.page_link('pages/1_movement_tool.py', label='Movement Tool', icon='🛡️')
+        st.page_link('pages/2_advance_help.py', label='Advance Help', icon='🛡️')
+        st.page_link('pages/3_city_calculator.py', label='City Calculator', icon='🛡️')
+        st.page_link('pages/4_calamities.py', label='Calamities', icon='🛡️')
+    st.title(f'🔥 Individual Checker')
+
 st.set_page_config(
     page_title="Game Assistance App",
     page_icon="🎲",
@@ -15,3 +25,6 @@ Welcome to the multi-functional game assistance app! Use the sidebar to navigate
 - City Calculator
 - Calamities Overview
 """)
+
+if __name__ == '__main__':
+    main()
