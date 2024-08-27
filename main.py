b9ff8942d349a79b6d5e0c9616c6b175abf33184
chrisvandijk <mail@chrisvandijk.nl>
2024-08-27 20:44:47 +0200

    Revert back to working version

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 import streamlit as st
 
-def main():
-    # builds the sidebar menu
-    with st.sidebar:
-        st.page_link('main.py', label='Welcome', icon='🔥')
-        st.page_link('pages/1_movement_tool.py', label='Movement Tool', icon='🛡️')
-        st.page_link('pages/2_advance_help.py', label='Advance Help', icon='🛡️')
-        st.page_link('pages/3_city_calculator.py', label='City Calculator', icon='🛡️')
-        st.page_link('pages/4_calamities.py', label='Calamities', icon='🛡️')
-    st.title(f'🔥 Individual Checker')
-
-'''st.set_page_config(
+st.set_page_config(
     page_title="Game Assistance App",
     page_icon="🎲",
     layout="wide",
-)'''
-
-'''# Main page content
+)
+# Main page content
 st.title("Game Assistance App")
 st.write("""
 Welcome to the multi-functional game assistance app! Use the sidebar to navigate between different tools: 
@@ -24,7 +13,4 @@ Welcome to the multi-functional game assistance app! Use the sidebar to navigate
 - Advancement Helper
 - City Calculator
 - Calamities Overview
-""")'''
-
-if __name__ == '__main__':
-    main()
+""")
